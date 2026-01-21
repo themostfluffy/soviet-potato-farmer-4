b8d77e667aa9d17934d35d490fbcbb0f1cd00e46
themostfluffy <Fuzzyfox9876@gmail.com>
2026-01-21 16:04:28 +0000

    nvm

---
+++ sovietpotato.js
@@ -0,0 +1,17 @@
+//the variables
+var money = 0;
+var vodka = 0;
+var potatoes = 0;
+var keg = 0;
+var happiness = 0;
+
+
+
+function setup() {
+  createCanvas(1500, 835);
+}
+function draw() {
+  background(0, 0, 0);
+
+
+}
